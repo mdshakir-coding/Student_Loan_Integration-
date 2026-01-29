2fbc8eac21d1db582a5dad4b219b20c4065b0c13
Md Saddam <mdsonu7860@outlook.com>
2026-01-29 17:16:45 +0530

    Comment

--- src/server.js
+++ src/server.js
@@ -3,14 +3,14 @@ dotenv.config();
 import app from "./app.js";
 
 // import these function
-import { syncInquirer } from "./controller/inquirer.controller.js";
-import { syncAffiliate } from "./controller/affiliate.controller.js";
-import { syncActivity } from "./controller/activity.controller.js";
-import { syncInvoices } from "./controller/invoices.controller.js";
-import { syncClients } from "./controller/clients.controller.js";
-import { syncOrders } from "./controller/orders.controller.js";
-import { syncTextMessages } from "./controller/textmessages.controller.js";
-import { syncEmails } from "./controller/emails.controller.js";
+// import { syncInquirer } from "./controller/inquirer.controller.js";
+// import { syncAffiliate } from "./controller/affiliate.controller.js";
+// import { syncActivity } from "./controller/activity.controller.js";
+// import { syncInvoices } from "./controller/invoices.controller.js";
+// import { syncClients } from "./controller/clients.controller.js";
+// import { syncOrders } from "./controller/orders.controller.js";
+// import { syncTextMessages } from "./controller/textmessages.controller.js";
+// import { syncEmails } from "./controller/emails.controller.js";
 
 console.log("Loaded API Token:", process.env.HUBSPOT_API_KEY);
 const PORT = process.env.PORT || 3400;
